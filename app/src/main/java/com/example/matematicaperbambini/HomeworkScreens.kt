1046973b7da8ceb5c40ea66e14065a20fc9c9412
davidesiro-dot <davide.siro@gmail.com>
2026-01-30 02:12:03 +0100

    Fix homework screen compose imports

--- app/src/main/java/com/example/matematicaperbambini/HomeworkScreens.kt
+++ app/src/main/java/com/example/matematicaperbambini/HomeworkScreens.kt
@@ -2,9 +2,11 @@ package com.example.matematicaperbambini
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.align
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
